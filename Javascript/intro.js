b3a8b86ccb8579765d342ec157b8b00bc5738052
Elly <misselly79@gmail.com>
2021-05-31 16:52:46 +0200

    Javascript Week 5

---
+++ Javascript/intro.js
@@ -0,0 +1,90 @@
+/*let letOefening = "Let text"
+let letOefening = "Let text Twee!"
+
+console.log(letOefening);*/
+
+
+/*let letOefening = "Let text"
+console.log(letOefening)
+
+letOefening = "Let text twee!"
+
+console.log(letOefening)*/
+
+/*const constOefening = "Const Text";
+console.log(constOefening)
+
+constOefening = "Const Text Twee!";
+console.log(constOefening)  */
+
+/*varOefening = "Var Text";
+console.log(varOefening)
+
+var varOefening = "Var Text Twee!";
+console.log(varOefening);
+
+var varOefening = "Var Text Drie";
+console.log(varOefening)*/
+
+// let typeQuestion = function calculateSomething() {}
+// console.log("This is a  " + typeof typeQuestion + ", with the value: " + typeQuestion)
+
+// let someNumber = 5;
+// let anotherNumber = "5";
+// console.log(someNumber + anotherNumber)
+
+// let someNumber = 5;
+// let anotherNumber = 5;
+// console.log(someNumber + anotherNumber)
+
+// let someNumber = 5;
+// let anotherNumber = 5;
+// console.log(someNumber == anotherNumber)
+
+// let someNumber = 5;
+// let anotherNumber = 5;
+// console.log(someNumber === anotherNumber)
+
+
+// let someNumber = 5;
+// let anotherNumber = 5;
+// console.log(someNumber !== anotherNumber)
+
+// let increment = 1;
+// console.log(increment++)
+// console.log(++increment)
+
+// let modulo = 24 % 10;
+// console.log(modulo)
+// let division = 24 / 10;
+// console.log(division)
+
+// let someNumber = "Tekst";
+// console.log(! someNumber)
+// let someNumber = undefined ;
+// console.log(!someNumber)
+
+// let operator = ("Hello");
+// let operator2 = (" Elly");
+// console.log(operator += operator2)
+
+let someKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+let some = "De poes van de buurman heeft kittens gehad!";
+let kittens = "Hij vraagt of wij nog kittens willen.";
+console.log(some += kittens);
+
+console.log(someKittens.toUpperCase());
+
+console.log(someKittens.indexOf("kittens"));
+
+console.log(someKittens.lastIndexOf("kittens"));
+
+console.log(someKittens.charAt(indexOf));
+
+Ik ben gebleven bij 2.1
+
+
+
+
+
+
