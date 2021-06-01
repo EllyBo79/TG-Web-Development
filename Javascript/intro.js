82a564f9ed2b356d230c233ab15a824170adfe02
Elly <misselly79@gmail.com>
2021-06-01 16:49:02 +0200

    Week 5

--- Javascript/intro.js
+++ Javascript/intro.js
@@ -68,22 +68,102 @@ console.log(varOefening)*/
 // let operator2 = (" Elly");
 // console.log(operator += operator2)
 
-let someKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
-let some = "De poes van de buurman heeft kittens gehad!";
-let kittens = "Hij vraagt of wij nog kittens willen.";
-console.log(some += kittens);
+// let someKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// let some = "De poes van de buurman heeft kittens gehad!";
+// let kittens = "Hij vraagt of wij nog kittens willen.";
+// console.log(some += kittens);
 
-console.log(someKittens.toUpperCase());
+// console.log(someKittens.toUpperCase());
 
-console.log(someKittens.indexOf("kittens"));
+// console.log(someKittens.indexOf("kittens"));
 
-console.log(someKittens.lastIndexOf("kittens"));
+// console.log(someKittens.lastIndexOf("kittens"));
 
-console.log(someKittens.charAt(indexOf));
+// console.log(someKittens.charAt(3));
 
-Ik ben gebleven bij 2.1
+// console.log(kittens.charAt(5))
 
+// let ellyBo="Elly is super Web Developer!";
+// console.log(ellyBo.substring(0, -8));
 
+// let ellyBo="Elly is super Web Developer!";
+// console.log(ellyBo.slice(0, -8));
+// *Output hier is : Elly is een super Web De
+
+// let someKittens = "Die nieuwe kittens zijn zo schattig!";
+// console.log(someKittens.split(" "));
+
+// let x = 4;
+// let y = 8;
+//     if (x==4 && y==8) {
+//         console.log("Result!")
+//         }    
+
+    // let x = 4;
+    // let y = 6;
+    //     if (x==4 || y==8) {
+    //         console.log("Result!")
+    //         }    
+
+            // let x = 4;
+            // let y = 8;
+            //  if (x==4 != y==8) {
+            //      console.log("No result!")
+            //  } else {
+            //          console.log("Result!")                 
+            //  }        
+
+//    let x = 41;
+//         if (x > 5 && x < 10) {
+//             console.log("Greater than 5 and smaller than 10. Hurray!")            
+//         }   else if (x > 11 && x < 20) {
+//             console.log("Greater than 11 and smaller than 20")
+//         }       else if (x == 21 || x == 23) {
+//             console.log("The number is 21 or 23")
+//         }           else if (x < 35 || x > 40 && x < 45) {
+//             console.log("The number is lower than 35  or between 40 and 45")
+//         }
+
+// let x = 20;
+//     if (x == 3) {
+//         console.log("Yes! Equal to 3!")
+//     } else if (x > 4 && x < 11) {
+//         console.log("Yes, Greater than 4!")
+//     }   else if (x > 11) {
+//         console.log("Yippie, Greater than 11!!!")
+//     }       else if (x < 3) {
+//         console.log("Smaller than 3")
+//     }
+
+let month = 13;
+    if (month == 1) {
+    console.log("Januari")
+    }   else if (month == 2) {
+        console.log("February")
+    }    else if (month == 3) {
+        console.log("March")
+    }    else if (month == 4) {
+        console.log("April")
+    }    else if (month == 5) {
+        console.log("May")
+    }    else if (month == 6) {
+        console.log("June")
+    }    else if (month == 7) {
+        console.log("July")
+    }    else if (month == 8) {
+        console.log("August")
+    }    else if (month == 9) {
+        console.log("September")
+    }    else if (month == 10) {
+        console.log("October")
+    }    else if (month == 11) {
+        console.log("November")
+    }    else if (month == 12) {
+        console.log("December")
+    }   else if (month > 12) {
+        console.log("Geen geldige maand")
+    }
+    
 
 
 
